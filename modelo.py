775e2e222ecdcc44ba35c5948aa1482ae61ede3d
noceive <nocivescience@gmail.com>
2023-05-19 02:41:04 -0400

    esta es en random

--- modelo.py
+++ modelo.py
@@ -1,3 +1,5 @@
+from manim import *
+import operator as op
 def my_func(*my_points, **kwargs):
     radius = kwargs.get("radius", 0.5)
 
@@ -5,7 +7,7 @@ def my_func(*my_points, **kwargs):
         result = np.array(ORIGIN)
         for center, strength in my_points:
             to_center = center-point
-            norm = get_norm(to_center)
+            norm = np.linalg.norm(to_center)
             if norm == 0:
                 continue
             elif norm < radius:
@@ -16,26 +18,24 @@ def my_func(*my_points, **kwargs):
             result += to_center
         return result
     return func
-
-
 class ChargeParticle(Dot):
     CONFIG = {
         "vector_field_config": {},
         "n_particles": 6,
         "anim_time": 5,
-        "buff": 0.5
+        "buff": 0.5,
+        "radio": 0.5
     }
-
     def __init__(self, sign=True, **kwargs):
         Dot.__init__(self, **kwargs)
         # digest_config(self,kwargs)
-        self.set_width(width=self.radio)
+        self.set(width=self.CONFIG['radio'])
         self.set_color(BLUE)
         if sign:
             TEX = "+"
         else:
             TEX = "-"
-        signo = TexMobject(TEX)
+        signo = Tex(TEX)
         self.add(signo)
 
 
@@ -47,14 +47,15 @@ class MiVectorField(Scene):
         },
         "charge_kwargs": {
             "radius": 0.3
-        }
+        },
+        "radio": 0.5
     }
 
     def construct(self):
         charges = self.get_my_charges(6)
         vector_field = self.get_force_field(charges)
-        self.play(ShowCreation(vector_field), **self.anim_kwargs)
-        self.play(ShowCreation(charges))
+        self.play(Create(vector_field), **self.CONFIG['anim_kwargs'])
+        self.play(Create(charges))
 
         def update_vector_field(vector_field):
             new_field = self.get_force_field(charges)
@@ -66,7 +67,7 @@ class MiVectorField(Scene):
             for particle in particles:
                 force = func(particle.get_center())
                 particle.center += particle.velocity*dt
-                for i, L in zip([0, 1], [FRAME_X_RADIUS, FRAME_Y_RADIUS]):
+                for i, L in zip([0, 1], [config['frame_width']/2, config['frame_height']/2]):
                     if abs(particle.center[i]) > L:
                         particle.center[i] = np.sign(particle.center[i])*L
                         particle.velocity[i] *= -1*op.mul(
@@ -74,13 +75,13 @@ class MiVectorField(Scene):
                 particle.shift(particle.velocity*dt)
         vector_field.add_updater(update_vector_field)
         charges.add_updater(update_particles)
-        self.wait(60)
+        self.wait(4)
 
     def get_my_charges(self, amount, atribute=None):
         position = np.array([
             [
-                FRAME_X_RADIUS*np.random.uniform(-1, 1),
-                FRAME_Y_RADIUS*np.random.uniform(-1, 1),
+                config['frame_width']/2*np.random.uniform(-1, 1),
+                config['frame_height']/2*np.random.uniform(-1, 1),
                 0
             ]
             for n in range(amount)
@@ -107,7 +108,10 @@ class MiVectorField(Scene):
     def get_force_field(self, mis_cargas):
         func = my_func(
             *list(zip(list(map(lambda x: x.get_center(), mis_cargas)),
-                      [p.charge for p in mis_cargas])), **self.charge_kwargs
+                      [p.charge for p in mis_cargas])), **self.CONFIG['charge_kwargs']
         )
-        vector_field = VectorField(func)
+        vector_field = ArrowVectorField(func)
+        # vector_field = ArrowVectorField(
+        #     func, x_range=[-7, 7, 1], y_range=[-4, 4, 1], length_func=lambda x: x / 2
+        # )
         return vector_field
--- vecotrial_field.py
+++ vecotrial_field.py
@@ -1,11 +1,12 @@
 from manim import *
+import operator as op
 import itertools as it
-def universal_func(*puntos,**kwargs):
-    radius=kwargs.get("radius",0.3)
-    def func(punto):
+def my_func(*my_points, **kwargs):
+    radius=kwargs.get("radius",0.5)
+    def func(point):
         result=np.array(ORIGIN)
-        for center, strength in puntos:
-            to_center=center-punto
+        for center,strength in my_points:
+            to_center=center-point
             norm=np.linalg.norm(to_center)
             if norm==0:
                 continue
@@ -23,69 +24,72 @@ class ChargeParticle(Dot):
         "n_particles":6,
         "anim_time":5,
         "buff":0.5,
-        "radio":0.3,
-        "color":[
-            "#FF0000",
-            "#FF7F00",
-            "#FFFF00",
-            "#00FF00",
-            "#0000FF",
+        "radio":0.5,
+    }
+    def __init__(self,color,sign=True,**kwargs):
+        Dot.__init__(self,**kwargs)
+        self.set(width=self.CONFIG['radio'])
+        self.set_color(color)
+        if sign:
+            TEX="+"
+        else:
+            TEX="-"
+        signo=Tex(TEX)
+        self.add(signo)
+class MyVectorField(Scene):
+    CONFIG={
+        'anim_kwargs':{
+            'run_time':0.4,
+            'rate_func':linear
+        },
+        'charge_kwargs':{
+            'radius':0.3
+        },
+        'radio':0.5,
+        "colors":[
+            BLUE,
+            RED,
+            GREEN,
+            YELLOW,
+            PURPLE,
+            ORANGE
         ]
     }
-    def __init__(self, sign=True, **kwargs):
-        Dot.__init__(self, **kwargs)
-        self.set(width=self.CONFIG["radio"])
-        self.set_color(next(it.cycle(self.CONFIG["color"])))
-class VectorialField(Scene):
-    CONFIG={
-        "anim_kwargs":{
-            "run_time":0.4,
-            "rate_func": linear
-        },
-        "charge_kwargs":{
-            "radius":0.3
-        },
-        "n_particles":6,
-    }
     def construct(self):
-        cargas=self.get_my_charge(self.CONFIG["n_particles"])
-        vector_field=self.get_force_field(cargas)
-        self.play(Create(cargas))
-        self.play(Create(vector_field),**self.CONFIG["anim_kwargs"])
+        charges=self.get_charges(12)
+        self.play(Create(charges),**self.CONFIG['anim_kwargs'])
         self.wait()
-    def get_my_charge(self, amount, attribute=None):
-        position= np.array([
+    def get_charges(self,amount):
+        positions = np.array([
             [
-                config["frame_width"]/2*np.random.uniform(-1,1),
-                config["frame_height"]/2*np.random.uniform(-1,1),
+                config['frame_width']/2*np.random.uniform(-1,1),
+                config['frame_height']/2*np.random.uniform(-1,1),
                 0
             ]
             for _ in range(amount)
         ])
-        cargas=VGroup()
-        for point in position:
-            valor=np.random.random()
-            carga=ChargeParticle(sign=attribute)
-            if valor<.5:
-                attribute=False
+        charges = VGroup()
+        colors = it.cycle(self.CONFIG['colors'])
+        for position in positions:
+            color = next(colors)
+            valor=np.random.choice([True,False])
+            if valor:
+                atributte=True
             else:
-                attribute=True
-            if attribute:
-                carga.charge=1
+                atributte=False
+            charge = ChargeParticle(color,atributte,**self.CONFIG['charge_kwargs'])
+            if atributte:
+                charge.carga=1
             else:
-                carga.charge=-1
-            carga.center=point
-            carga.move_to(carga.center)
-            carga.velocity=rotate_vector(
-                np.random.random()*RIGHT*config["frame_width"]/2,
-                np.random.random()*TAU
+                charge.carga=-1
+            charge.center = position
+            charge.move_to(charge.center)
+            charges.velocity=rotate_vector(
+                np.random.random(3)*RIGHT,
+                np.random.uniform(0,2*np.pi)
             )
-            cargas.add(carga)
-        return cargas
-    def get_force_field(self, charges):
-        func=universal_func(
-            *list(zip(list(map(lambda x: x.get_center(), charges)),[p.charge for p in charges])),
-            **self.CONFIG["charge_kwargs"]
-        )
-        vector_field=VectorField(func)
-        return vector_field
+            charges.add(charge)
+        return charges
+            
+                
+        
